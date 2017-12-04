3e558e83e455169cc919d33203f19b6085ac3431
yaron <yaron57@gmail.com>
2017-12-04 17:36:16 +0300

    Various improvements

--- SpecialCloneDiff.php
+++ SpecialCloneDiff.php
@@ -3,6 +3,10 @@
 class SpecialCloneDiff extends SpecialPage {
 	private $categories, $namespace;
 
+	const LOCAL_ONLY = 0;
+	const REMOTE_ONLY = 1;
+	const IN_BOTH = 2;
+
 	public function __construct() {
 		parent::__construct( 'CloneDiff', 'clonediff' );
 	}
@@ -29,53 +33,101 @@ class SpecialCloneDiff extends SpecialPage {
 		$this->displayInitialForm();
 	}
 
-	function importAndDisplayResults() {
+	function displayInitialForm( $warning_msg = null ) {
 		global $wgCloneDiffWikis;
 
-		wfProfileIn( __METHOD__ );
-
 		$out = $this->getOutput();
-		$user = $this->getUser();
-		$request = $this->getRequest();
 
-		$selectedWiki = $request->getVal( 'wikinum' );
-		$apiURL = $wgCloneDiffWikis[$selectedWiki]['API URL'];
-
-		$replacement_params['user_id'] = $user->getId();
-		//$replacement_params['edit_summary'] = $this->msg( 'clonediff-editsummary' )->inContentLanguage()->plain();
-
-		$pagesToImport = [];
-		foreach ( $request->getValues() as $key => $value ) {
-			if ( $value == '1' && $key !== 'import' && $key !== 'wikinum' ) {
-				$pagesToImport[] = $key;
-			}
-		}
-
-		$jobs = [];
-		$remotePageData = $this->getRemoteDataForPageSet( $apiURL, $pagesToImport );
-		foreach ( $remotePageData as $remotePage ) {
-			$pageName = $remotePage->title;
-			$remotePageRev = $remotePage->revisions[0];
-			$replacement_params['page_text'] = $remotePageRev->{'*'}; // ???
-
-			$title = Title::newFromText( $pageName );
-			if ( $title !== null ) {
-				$jobs[] = new ImportFromCloneJob( $title, $replacement_params );
-			}
-		}
-
-		JobQueueGroup::singleton()->push( $jobs );
-
-		$count = $this->getLanguage()->formatNum( count( $jobs ) );
-		$out->addWikiMsg( 'clonediff-success', $count );
-
-		// Link back
 		$out->addHTML(
-			Linker::link( $this->getTitle(),
-				$this->msg( 'clonediff-return' )->escaped() )
+			Xml::openElement(
+				'form',
+				[
+					'id' => 'powersearch',
+					'action' => $this->getTitle()->getFullUrl(),
+					'method' => 'post'
+				]
+			) . "\n" .
+			Html::hidden( 'title', $this->getTitle()->getPrefixedText() ) .
+			Html::hidden( 'continue', 1 )
 		);
 
-		wfProfileOut( __METHOD__ );
+		if ( count( $wgCloneDiffWikis ) == 1 ) {
+			$wiki = $wgCloneDiffWikis[0];
+			$wikiLink = Html::element( 'a', [ 'href' => $wiki['URL'] ], $wiki['name'] );
+			$out->addHTML( "<p><b>Remote wiki: $wikiLink.</b></p>" );
+		} else {
+			// Make a dropdown
+			$dropdownHTML = '<select name="remoteWiki">';
+			foreach ( $wgCloneDiffWikis as $i => $cloneDiffWiki ) {
+				$dropdownHTML .= '<option value="' . $i . '">' . $cloneDiffWiki['name'] . '</option>';
+			}
+			$dropdownHTML .= '</select>';
+			$out->addHTML( "<p><b>Remote wiki:</b> $dropdownHTML</p>" );
+		}
+
+		if ( is_null( $warning_msg ) ) {
+			$out->addWikiMsg( 'clonediff-docu' );
+		} else {
+			$out->wrapWikiMsg(
+				"<div class=\"errorbox\">\n$1\n</div><br clear=\"both\" />",
+				$warning_msg
+			);
+		}
+
+		$out->addHTML( '<p>' . Xml::checkLabel( 'Include pages that only exist locally', "viewLocalOnly", "viewLocalOnly", true ) . '</p>' );
+		$out->addHTML( '<p>' . Xml::checkLabel( 'Include pages that only exist remotely', "viewRemoteOnly", "viewRemoteOnly", true ) . '</p>' );
+
+		// The interface is heavily based on the one in Special:Search.
+		$namespaces = array( 'all' => '<em>All</em>' ) +
+			SearchEngine::searchableNamespaces();
+		$nsText = "\n";
+		foreach ( $namespaces as $ns => $name ) {
+			if ( '' == $name ) {
+				$name = $this->msg( 'blanknamespace' )->text();
+			}
+			$name = str_replace( '_', ' ', $name );
+			$nsButton = '<label><input type="radio" name="namespace"' .
+				' value="' . $ns . '"';
+			if ( $ns === 'all' ) {
+				$nsButton .= ' checked="checked"';
+			}
+			$nsButton .= '" />' . $name . '</label>';
+			$nsText .= '<span style="float: left; width: 150px;">' .
+				$nsButton . "</span>\n";
+		}
+		$out->addHTML(
+			"<fieldset id=\"mw-searchoptions\">\n" .
+			Xml::tags( 'h4', null, "Search in namespace:" ) .
+			"$nsText\n</fieldset>"
+		);
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$categorylinks = $dbr->tableName( 'categorylinks' );
+		$res = $dbr->query( "SELECT DISTINCT cl_to FROM $categorylinks" );
+		$categories = array();
+		while ( $row = $dbr->fetchRow( $res ) ) {
+			$categories[] = str_replace( '_', ' ', $row[0] );
+		}
+		$dbr->freeResult( $res );
+		sort( $categories );
+
+		//$tables = $this->categoryTables( $categories );
+		$categoriesText = '';
+		foreach ( $categories as $cat ) {
+			$categoryCheck = Xml::checkLabel( $cat, "categories[$cat]", "mw-search-category-$cat" );
+			$categoriesText .= '<span style="float: left; width: 170px; padding-right: 15px;">' .
+				$categoryCheck . '</span>';
+		}
+		$out->addHTML(
+			"<fieldset id=\"mw-searchoptions\">\n" .
+			Xml::tags( 'h4', null, "Search in categories:" ) .
+			"$categoriesText\n</fieldset>"
+		);
+
+		$out->addHTML(
+			Xml::submitButton( $this->msg( 'clonediff-continue' )->parse() ) .
+			Xml::closeElement( 'form' )
+		);
 	}
 
 	public function getLocalPages() {
@@ -130,7 +182,9 @@ class SpecialCloneDiff extends SpecialPage {
 					throw new MWException( "API at $remoteAPIURL is not responding." );
 				}
 				$apiResultData = json_decode( $apiResult );
-				//$remotePageData = get_object_vars( $apiResultData->query->categorymembers );
+				if ( isset( $apiResultData->error ) ) {
+					throw new MWException( "Error accessing remote API: code = " . $apiResultData->error->code . ", message = " . $apiResultData->error->info . "." );
+				}
 				$remotePageData = $apiResultData->query->categorymembers;
 				foreach ( $remotePageData as $remotePage ) {
 					$remotePages[] = $remotePage->title;
@@ -153,6 +207,9 @@ class SpecialCloneDiff extends SpecialPage {
 				throw new MWException( "API at $remoteAPIURL is not responding." );
 			}
 			$apiResultData = json_decode( $apiResult );
+			if ( isset( $apiResultData->error ) ) {
+				throw new MWException( "Error accessing remote API: code = " . $apiResultData->error->code . ", message = " . $apiResultData->error->info . "." );
+			}
 			$remotePageData = $apiResultData->query->allpages;
 			foreach ( $remotePageData as $remotePage ) {
 				$remotePages[] = $remotePage->title;
@@ -160,6 +217,20 @@ class SpecialCloneDiff extends SpecialPage {
 		} while ( count( $remotePageData ) == 500 );
 
 		return $remotePages;
+	}
+
+	function displayDiffHeader( $pageLink, $timestamp, $lastEditor ) {
+		$diffHeader = $pageLink;
+		if ( $timestamp != null ) {
+			$lang = $this->getLanguage();
+			$user = $this->getUser();
+			$timestampText = $lang->userTimeAndDate( $timestamp, $user );
+			$diffHeader .= '<div id="mw-diff-otitle1"><strong>' . $timestampText . '</strong></div>';
+		}
+		if ( $lastEditor != null ) {
+			$diffHeader .= '<div id="mw-diff-otitle2">Last edited by: ' . $lastEditor . '</div>';
+		}
+		return $diffHeader;
 	}
 
 	function displayDiffsForm() {
@@ -183,6 +254,9 @@ class SpecialCloneDiff extends SpecialPage {
 			$selectedWiki = $request->getVal( 'remoteWiki' );
 		}
 		$apiURL = $wgCloneDiffWikis[$selectedWiki]['API URL'];
+
+		$viewPagesOnlyInLocal = $request->getCheck( 'viewLocalOnly' );
+		$viewPagesOnlyInRemote = $request->getCheck( 'viewRemoteOnly' );
 
 		$out->addModuleStyles( 'mediawiki.diff.styles' );
 
@@ -216,19 +290,23 @@ class SpecialCloneDiff extends SpecialPage {
 
 		$allPages = array();
 
-		$pagesOnlyInLocal = array_diff( $localPages, $remotePages );
-		foreach ( $pagesOnlyInLocal as $pageName ) {
-			$allPages[$pageName] = 0;
+		if ( $viewPagesOnlyInLocal ) {
+			$pagesOnlyInLocal = array_diff( $localPages, $remotePages );
+			foreach ( $pagesOnlyInLocal as $pageName ) {
+				$allPages[$pageName] = self::LOCAL_ONLY;
+			}
 		}
 
-		$pagesOnlyInRemote = array_diff( $remotePages, $localPages );
-		foreach ( $pagesOnlyInRemote as $pageName ) {
-			$allPages[$pageName] = 1;
+		if ( $viewPagesOnlyInRemote ) {
+			$pagesOnlyInRemote = array_diff( $remotePages, $localPages );
+			foreach ( $pagesOnlyInRemote as $pageName ) {
+				$allPages[$pageName] = self::REMOTE_ONLY;
+			}
 		}
 
 		$pagesInBoth = array_intersect( $localPages, $remotePages );
 		foreach ( $pagesInBoth as $pageName ) {
-			$allPages[$pageName] = 2;
+			$allPages[$pageName] = self::IN_BOTH;
 		}
 
 		ksort( $allPages );
@@ -251,6 +329,7 @@ class SpecialCloneDiff extends SpecialPage {
 		foreach ( $localAndRemoteData as $pageName => $curPageData ) {
 			$localText = $curPageData['localText'];
 			$remoteText = $curPageData['remoteText'];
+			$title = null;
 			if ( $localText != null ) {
 				$title = Title::newFromText( $pageName );
 			}
@@ -275,10 +354,24 @@ class SpecialCloneDiff extends SpecialPage {
 				if ( $diffText !== false ) {
 					$diffText = $diffEngine->localiseLineNumbers( $diffText );
 				}
-$url = '';
-				$outsideLink = Html::element( 'a', [ 'href' => $url ], 'Remote version' );
-				$localLink = Linker::link( $title, 'Local version' );
-				$html .= $diffEngine->addHeader( $diffText, $outsideLink, $localLink );
+
+				$url = str_replace( 'api.php', 'index.php?title=' . str_replace( ' ', '_', $pageName ), $apiURL );
+				$remoteLink = Html::element( 'a', [ 'href' => $url ], 'Remote version' );
+				$remoteHeader = $this->displayDiffHeader( $remoteLink, $curPageData['remoteTime'], $curPageData['remoteUser'] );
+
+				if ( $title == null ) {
+					$localLink = 'Local version (nonexistent)';
+					$time = null;
+					$user = null;
+				} else {
+					$localLink = Linker::link( $title, 'Local version' );
+					$rev = Revision::newFromTitle( $title );
+					$time = $rev->getTimestamp();
+					$user = $rev->getUserText();
+				}
+				$localHeader = $this->displayDiffHeader( $localLink, $time, $user );
+
+				$html .= $diffEngine->addHeader( $diffText, $remoteHeader, $localHeader );
 			}
 			$out->addHTML( $html );
 			$out->addHTML( "<br /><hr /><br />\n" );
@@ -313,7 +406,7 @@ $url = '';
 		$pagesNotInRemoteWiki = array();
 		$pagesInRemoteWiki = array();
 		foreach( $pageSet as $pageName => $status ) {
-			if ( $status == 0 ) {
+			if ( $status == self::LOCAL_ONLY ) {
 				$pagesNotInRemoteWiki[] = $pageName;
 			} else {
 				$pagesInRemoteWiki[] = $pageName;
@@ -325,7 +418,7 @@ $url = '';
 		foreach ( $remotePageData as $remotePage ) {
 			$curPageData = [];
 			$pageName = $remotePage->title;
-			if ( $pageSet[$pageName] == 2 ) {
+			if ( $pageSet[$pageName] == self::IN_BOTH ) {
 				$localTitle = Title::newFromText( $pageName );
 				$rev = Revision::newFromTitle( $localTitle );
 				$localContent = $rev->getContent();
@@ -339,10 +432,9 @@ $url = '';
 			$remoteText = $remotePageRev->{'*'}; // ???
 			$curPageData['remoteText'] = $remoteText;
 
-			if ( $localText == $remoteText ) {
-				//$pageData[$pageName] = 'no change';
-			} else {
-				$curPageData['time'] = $remotePageRev->timestamp;
+			if ( $localText !== $remoteText ) {
+				$curPageData['remoteTime'] = $remotePageRev->timestamp;
+				$curPageData['remoteUser'] = $remotePageRev->user;
 			}
 			$allPageData[$pageName] = $curPageData;
 		}
@@ -397,98 +489,53 @@ $url = '';
 		return $params;
 	}
 
-	function displayInitialForm( $warning_msg = null ) {
+	function importAndDisplayResults() {
 		global $wgCloneDiffWikis;
 
+		wfProfileIn( __METHOD__ );
+
 		$out = $this->getOutput();
+		$user = $this->getUser();
+		$request = $this->getRequest();
 
-		$out->addHTML(
-			Xml::openElement(
-				'form',
-				[
-					'id' => 'powersearch',
-					'action' => $this->getTitle()->getFullUrl(),
-					'method' => 'post'
-				]
-			) . "\n" .
-			Html::hidden( 'title', $this->getTitle()->getPrefixedText() ) .
-			Html::hidden( 'continue', 1 )
-		);
+		$selectedWiki = $request->getVal( 'wikinum' );
+		$apiURL = $wgCloneDiffWikis[$selectedWiki]['API URL'];
 
-		if ( count( $wgCloneDiffWikis ) == 1 ) {
-			$wiki = $wgCloneDiffWikis[0];
-			$wikiLink = Html::element( 'a', [ 'href' => $wiki['URL'] ], $wiki['name'] );
-			$out->addHTML( "<p><b>Remote wiki: $wikiLink.</b></p>" );
-		} else {
-			// Make a dropdown
-			$dropdownHTML = '<select name="remoteWiki">';
-			foreach ( $wgCloneDiffWikis as $i => $cloneDiffWiki ) {
-				$dropdownHTML .= '<option value="' . $i . '">' . $cloneDiffWiki['name'] . '</option>';
+		$replacement_params['user_id'] = $user->getId();
+		//$replacement_params['edit_summary'] = $this->msg( 'clonediff-editsummary' )->inContentLanguage()->plain();
+
+		$pagesToImport = [];
+		foreach ( $request->getValues() as $key => $value ) {
+			if ( $value == '1' && $key !== 'import' && $key !== 'wikinum' ) {
+				$pagesToImport[] = $key;
 			}
-			$dropdownHTML .= '</select>';
-			$out->addHTML( "<p><b>Remote wiki:</b> $dropdownHTML</p>" );
 		}
 
-		if ( is_null( $warning_msg ) ) {
-			$out->addWikiMsg( 'clonediff-docu' );
-		} else {
-			$out->wrapWikiMsg(
-				"<div class=\"errorbox\">\n$1\n</div><br clear=\"both\" />",
-				$warning_msg
-			);
-		}
+		$jobs = [];
+		$remotePageData = $this->getRemoteDataForPageSet( $apiURL, $pagesToImport );
+		foreach ( $remotePageData as $remotePage ) {
+			$pageName = $remotePage->title;
+			$remotePageRev = $remotePage->revisions[0];
+			$replacement_params['page_text'] = $remotePageRev->{'*'}; // ???
 
-		// The interface is heavily based on the one in Special:Search.
-		$namespaces = array( 'all' => '<em>All</em>' ) +
-			SearchEngine::searchableNamespaces();
-		$nsText = "\n";
-		foreach ( $namespaces as $ns => $name ) {
-			if ( '' == $name ) {
-				$name = $this->msg( 'blanknamespace' )->text();
+			$title = Title::newFromText( $pageName );
+			if ( $title !== null ) {
+				$jobs[] = new ImportFromCloneJob( $title, $replacement_params );
 			}
-			$name = str_replace( '_', ' ', $name );
-			$nsButton = '<label><input type="radio" name="namespace"' .
-				' value="' . $ns . '"';
-			if ( $ns === 'all' ) {
-				$nsButton .= ' checked="checked"';
-			}
-			$nsButton .= '" />' . $name . '</label>';
-			$nsText .= '<span style="float: left; width: 150px;">' .
-				$nsButton . "</span>\n";
 		}
+
+		JobQueueGroup::singleton()->push( $jobs );
+
+		$count = $this->getLanguage()->formatNum( count( $jobs ) );
+		$out->addWikiMsg( 'clonediff-success', $count );
+
+		// Link back
 		$out->addHTML(
-			"<fieldset id=\"mw-searchoptions\">\n" .
-			Xml::tags( 'h4', null, "Search in namespace:" ) .
-			"$nsText\n</fieldset>"
+			Linker::link( $this->getTitle(),
+				$this->msg( 'clonediff-return' )->escaped() )
 		);
 
-		$dbr = wfGetDB( DB_SLAVE );
-		$categorylinks = $dbr->tableName( 'categorylinks' );
-		$res = $dbr->query( "SELECT DISTINCT cl_to FROM $categorylinks" );
-		$categories = array();
-		while ( $row = $dbr->fetchRow( $res ) ) {
-			$categories[] = str_replace( '_', ' ', $row[0] );
-		}
-		$dbr->freeResult( $res );
-		sort( $categories );
-
-		//$tables = $this->categoryTables( $categories );
-		$categoriesText = '';
-		foreach ( $categories as $cat ) {
-			$categoryCheck = Xml::checkLabel( $cat, "categories[$cat]", "mw-search-category-$cat" );
-			$categoriesText .= '<span style="float: left; width: 170px; padding-right: 15px;">' .
-				$categoryCheck . '</span>';
-		}
-		$out->addHTML(
-			"<fieldset id=\"mw-searchoptions\">\n" .
-			Xml::tags( 'h4', null, "Search in categories:" ) .
-			"$categoriesText\n</fieldset>"
-		);
-
-		$out->addHTML(
-			Xml::submitButton( $this->msg( 'clonediff-continue' )->parse() ) .
-			Xml::closeElement( 'form' )
-		);
+		wfProfileOut( __METHOD__ );
 	}
 
 	protected function getGroupName() {
