2e322fc0380af8fc61b46282da0641d8f7b072c9
Umherirrender <umherirrender_de.wp@web.de>
2018-03-16 20:13:20 +0100

    Add qqq message description
    Change-Id: Ida138b477ca42b21177acc3f9ea6e30493048b21

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint' /*, 'banana'*/ ] );
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
